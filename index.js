0221d0308f718eb88d3a655e8ec30074222961ee
Mohammedchandpasha <mohammedpasha7865@gmail.com>
2022-11-10 16:25:00 +0000

    boxes

---
+++ index.js
@@ -0,0 +1,25 @@
+const Box = (props) => {
+  //  Write your code here.
+  const { className, boxText } = props;
+  return (
+    <div className={`box ${className}`}>
+      <p>{boxText}</p>
+    </div>
+  );
+};
+
+const element = (
+  //  Write your code here.
+  <div className="bg-container">
+    <h1 className="box-heading">Boxes</h1>
+    <div className="boxes-container">
+      <Box className="small-box" boxText="Small" />
+
+      <Box className="medium-box" boxText="Medium" />
+
+      <Box className="large-box" boxText="Large" />
+    </div>
+  </div>
+);
+
+ReactDOM.render(element, document.getElementById("root"));
